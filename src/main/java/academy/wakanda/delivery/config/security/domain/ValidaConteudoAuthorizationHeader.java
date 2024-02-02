fc4caee9eaa43ab9d734298d7758af2ec6a50710
Manoel Mathias <dev.mathiasx@gmail.com>
2024-02-02 16:47:58 -0300

    implementa camadas de autenticacao e configuracao do spring security.

---
+++ src/main/java/academy/wakanda/delivery/config/security/domain/ValidaConteudoAuthorizationHeader.java
@@ -0,0 +1,10 @@
+package academy.wakanda.delivery.config.security.domain;
+
+import java.util.function.Predicate;
+
+public class ValidaConteudoAuthorizationHeader implements Predicate<String> {
+    @Override
+    public boolean test(String ConteudoAuthorizationHeader) {
+        return !ConteudoAuthorizationHeader.isEmpty() && ConteudoAuthorizationHeader.startsWith("Bearer");
+    }
+}
---
+++ src/main/java/academy/wakanda/delivery/config/security/service/AutenticacaoSecurityService.java
@@ -0,0 +1,30 @@
+package academy.wakanda.delivery.config.security.service;
+
+import java.util.Optional;
+
+import academy.wakanda.delivery.credencial.application.repository.CredencialRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+public class AutenticacaoSecurityService implements UserDetailsService {
+    @Autowired
+    private final CredencialRepository credencialRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        log.info("[inicia] AutenticacaoSecurityService - buscando credencial pelo usuario");
+        var credencial = credencialRepository.buscaCredencialPorEmail(email);
+        log.info("[finaliza] AutenticacaoSecurityService - buscando credencial pelo usuario");
+        return Optional.ofNullable(credencial).orElseThrow(() -> new RuntimeException("Não existe credencial para o Usuário informado!"));
+    }
+}
