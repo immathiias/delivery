6db370da60945d3a85432910ae186cae406193ce
Manoel Mathias <dev.mathiasx@gmail.com>
2024-02-02 16:38:26 -0300

    implementa camada domain autenticacao

---
+++ src/main/java/academy/wakanda/delivery/autenticacao/domain/Token.java
@@ -0,0 +1,15 @@
+package academy.wakanda.delivery.autenticacao.domain;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+@Getter
+@Builder
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class Token {
+    private String token;
+    private String tipo;
+}
