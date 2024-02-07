6f1f0aade24a8b559df71a46095f99068e554fec
Manoel Mathias <dev.mathiasx@gmail.com>
2024-02-06 21:18:41 -0300

    finaliza camada domain pedido

--- src/main/java/academy/wakanda/delivery/pedido/domain/Pedido.java
+++ src/main/java/academy/wakanda/delivery/pedido/domain/Pedido.java
@@ -1,5 +1,6 @@
 package academy.wakanda.delivery.pedido.domain;
 
+import academy.wakanda.delivery.cliente.domain.Endereco;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -7,6 +8,7 @@ import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -16,4 +18,12 @@ import java.util.UUID;
 public class Pedido {
     @Id
     private UUID idPedido;
+    private UUID idCliente;
+    private String produto;
+    private String detalhesPedido;
+    private Endereco idEnderecoEntrega;
+
+    private LocalDateTime dataHoraCriacaoDoPedido;
+    private LocalDateTime dataHoraAtuzaliacaoDoPedido;
+
 }
